511b669b6c38f2c8c0c6b3afea1cab9cc616c0ea
Metkiyd <sviridenko.dev@gmail.com>
2022-12-08 14:51:05 +0300

    upd result

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -3,22 +3,6 @@ import styles from './styles.module.scss'
 
 const Result = ({doneTasks}) => {
 
-  // useEffect(() => {
-  //   console.log('render always')
-  // })
-  //
-  // useEffect(() => {
-  //   console.log('===>Result mounted')
-  //
-  //   return () => {
-  //     console.log('Unmounted')
-  //   }
-  // }, [])
-  //
-  // useEffect(() => {
-  //   console.log('done render')
-  // }, [doneTasks])
-
   return (
     <footer className={styles.done}>
       <h1 className='form'>DONE:{doneTasks}</h1>
