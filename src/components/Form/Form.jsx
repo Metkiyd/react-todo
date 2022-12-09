40507bdbd514f14bf2cd7f7bb1df5b65f718c414
Metkiyd <sviridenko.dev@gmail.com>
2022-12-09 10:40:06 +0300

    Revert "reset commits mixed"
    This reverts commit f27844048fc06aa93ea73193ebf2946c20cb4992.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,7 +7,6 @@ const Form = () => {
   const [todo, setTodo] = useState('');
   const {todos, setTodos} = useContext(Context);
   // fsa
-  // fdsf
 
   const addTodo = text => {
     const newTodos = [...todos, {
